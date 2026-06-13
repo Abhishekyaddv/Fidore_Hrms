1a31b81e87997339b3b45944543297071788e649
Abhishek Yadav <ay6293347@gmail.com>
2026-06-13 11:35:54 +0530

    add PWA support

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+    <link rel="manifest" href="/manifest.json">
+        <meta name="theme-color" content="#1A3A2E">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-title" content="Fidore HRMS">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -14,6 +19,11 @@
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
+    <script>
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js');
+    }
+    </script>
     <body class="font-sans antialiased">
         @inertia
     </body>
